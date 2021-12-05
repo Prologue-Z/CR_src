e27db9417572d35cb695409bcc83937c74bcd8b4
Zhang Xu <tjuzx2016@126.com>
2021-12-05 11:49:01 +0800

    1

--- CANTest/src/CANMotor_Node.cpp
+++ CANTest/src/CANMotor_Node.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
     ros::NodeHandle nh("~");//实例化节点, 节点进程句柄
     CANMotor_NS::CANMotor MotorTest(nh);
 
-    signed short velocity[3] = {1000,1000,1000};
+    signed int velocity[3] = {1000,1000,1000};
     
     MotorTest.SetModeID(2);
     MotorTest.SetVelocity(velocity);
