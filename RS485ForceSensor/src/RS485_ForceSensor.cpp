078e3c92cfa751685c4400d11fa5c67b37fd2264
Zhang Xu <tjuzx2016@126.com>
2021-12-05 14:55:00 +0800

    1

--- RS485ForceSensor/src/RS485_ForceSensor.cpp
+++ RS485ForceSensor/src/RS485_ForceSensor.cpp
@@ -10,15 +10,27 @@
 
 #include <RS485ForceSensor/RS485_ForceSensor.h>
 
+#include <RS485ForceSensor/CommonFuncition.h>
+
+#include <stdlib.h>
+#include <string.h>
+
 namespace ForceSensor{
 
     //private
     int RS485_ForceSensor::AddFrame(MSG_CMD cmd,int data){
         unsigned char *pNode = NULL;
-        unsigned char aFrameHead[8] = {0};
-        swith
-
-
+        unsigned char NewFrame[8] = {0xAA,0xAA,0xAA,0x01};
         
+        NewFrame[SFI_Cmd] = cmd;
+        CommonFunction::ShortToChar(data, &NewFrame[SFI_Data]);
+
+        NewFrame[SFI_Check] = CommonFunction::BCC(NewFrame,7);
+
+        pNode = (unsigned char *)malloc(sizeof(NewFrame)); /* 发送线程会free掉它 */
+        memcpy(pNode, NewFrame, sizeof(NewFrame));
+
+        SendDataList.push_back(NewFrame);
+        return 0;
     }
 }
