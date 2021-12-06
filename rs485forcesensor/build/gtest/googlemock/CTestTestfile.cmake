9f7ca8be976eb81564c4e68187e4d613249920d5
Zhang Xu <tjuzx2016@126.com>
2021-12-06 12:31:08 +0800

    1

--- rs485forcesensor/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/zhangxu/catkin_ws/src/rs485forcesensor/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
