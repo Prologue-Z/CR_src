d1b38cca2f2d6dbcff6ed65caa9ad9b609fb35bb
Zhang Xu <tjuzx2016@126.com>
2021-12-24 10:50:10 +0800

    20211224

---
+++ continuumrobot/src/Class_CANMotor.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file Class_CANMotor.cpp
+ * @brief driver model:IDS-306  usbCAN model:GCAN USBCAN-Mini
+ * 
+ * @author Zhang Xu (tjuzx2016@126.com)
+ * @date 2021-12-24
+ * 
+ * @copyright Copyright (c) 2021 Key Laboratory of Mechanism Theory and Equipment Design of the Ministry of Education, School of Mechanical Engineering, Tianjin University, China
+ */
+
--- continuumrobot/src/continuumrobot_node.cpp
+++ continuumrobot/src/continuumrobot_node.cpp
@@ -55,7 +55,39 @@ int main(int argc, char **argv)
 		CloseDevice(nDeviceType, nDeviceInd);  
 		return -1; 
 	}
-	ROS_INFO("start can succ"); 
+	ROS_INFO("start can succ");
+
+	//clear buffer
+	dwRel = ClearBuffer(nDeviceType, nDeviceInd, nCANInd);
+	if (dwRel == STATUS_ERR){  
+		ROS_ERROR("fail clear buffer");
+		CloseDevice(nDeviceType, nDeviceInd);  
+		return -1; 
+	}
+	ROS_INFO("clear buffer successfully");
+
+	//init canobj
+	CAN_OBJ SendMotor[3];
+	BYTE init[8] = {0x00,0x1a,0xff,0x00,0x00,0xff,0x00,0x00};
+	enum DATA_INDEX{
+		DI_Head = 0,
+		DI_FunctionCode = 1,
+		DI_Register1 = 2,
+		DI_Data1 = 3,
+		DI_Register2 = 5,
+		DI_Data2 = 6,
+    };
+	
+	for(int i=0;i<3;i++){
+		SendMotor[i].ID = i+1;
+		SendMotor[i].DataLen = 8;
+		SendMotor[i].SendType = 0;
+		SendMotor[i].RemoteFlag = 0;
+		SendMotor[i].ExternFlag = 0;
+	}
+
     
 	CloseDevice(nDeviceType, nDeviceInd);
+	ROS_INFO("close can");
+
 }
