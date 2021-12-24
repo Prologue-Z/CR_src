f715877b0d227e30d5c1ac44d5ab70c01c5084f4
Zhang Xu <tjuzx2016@126.com>
2021-12-24 10:50:19 +0800

    20211224

---
+++ continuumrobot/include/continuumrobot/Class_CANMotor.h
@@ -0,0 +1,9 @@
+/**
+ * @file Class_CANMotor.h
+ * @brief driver model:IDS-306  usbCAN model:GCAN USBCAN-Mini
+ * 
+ * @author Zhang Xu (tjuzx2016@126.com)
+ * @date 2021-12-24
+ * 
+ * @copyright Copyright (c) 2021 Key Laboratory of Mechanism Theory and Equipment Design of the Ministry of Education, School of Mechanical Engineering, Tianjin University, China
+ */
