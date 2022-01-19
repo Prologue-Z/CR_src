32ecfdac9b1b6ad68b176682ebbb48635b3f92f7
Zhang Xu <tjuzx2016@126.com>
2022-01-19 21:38:21 +0800

    delete  redundancy packages.

--- ContinuumRobot_DataCollection/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhangxu/catkin_ws/src/ContinuumRobot_DataCollection
-# Build directory: /home/zhangxu/catkin_ws/src/ContinuumRobot_DataCollection/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
