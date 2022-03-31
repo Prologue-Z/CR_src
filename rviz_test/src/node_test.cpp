bf7fdeee0e7868be806e72b58768c9ef651c1c1c
Zhang Xu <tjuzx2016@126.com>
2022-03-31 20:37:11 +0800

    1

---
+++ rviz_test/src/node_test.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file node_test.cpp
+ * @brief 测试rviz显示连续体机器人
+ * 
+ * @author Zhang Xu (tjuzx2016@126.com)
+ * @date 2022-03-31
+ * 
+ * @copyright Copyright (c) 2022 Key Laboratory of Mechanism Theory and Equipment Design of the Ministry of Education, School of Mechanical Engineering, Tianjin University, China
+ */
+
